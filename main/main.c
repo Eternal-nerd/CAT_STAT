11007c296276f436c46a45a3b5416dda8ed0ae35
Myles Master <myles1551@gmail.com>
2025-08-08 23:02:40 -0500

    battery measure volts incomplete

--- main/main.c
+++ main/main.c
@@ -5,15 +5,42 @@
 #include "driver/spi_common.h"
 #include "esp_log.h"
 #include "sdkconfig.h"
+#include "esp_adc/adc_oneshot.h"
+
+//ADC2 Channels
+#define ADC_CHANNEL         ADC_CHANNEL_0
 
 // accelerometer stuff
 
 static const char *TAG = "CAT_STAT";
 
+static int adc_raw;
+
 void app_main(void) {
-    
-    while (1) {
-        ESP_LOGI(TAG, "Hello world");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+   
+	//-------------ADC2 Init---------------//
+    adc_oneshot_unit_handle_t adc_handle;
+    adc_oneshot_unit_init_cfg_t init_config = { 
+		.unit_id = ADC_UNIT_2,
+		.ulp_mode = ADC_ULP_MODE_DISABLE,
+	};
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
+
+	//-------------ADC2 Config---------------//
+    adc_oneshot_chan_cfg_t config = {
+        .atten = ADC_ATTEN_DB_12,
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, ADC_CHANNEL, &config));
+
+	for (;;) {
+        // loops every 0.1 seconds
+		ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, ADC_CHANNEL, &adc_raw));
+		
+		printf("raw_adc: %d\n", adc_raw);
+		
+		// wait 4 ms per degree rotated (just setting to 600 tho)
+        vTaskDelay(pdMS_TO_TICKS(600));
     }
+
 }
